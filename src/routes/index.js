5f3257a106e9f766858d2c172c8db9f0e72d7b6a
yosep <yosepalexsander9@gmail.com>
2021-10-28 10:03:39 +0700

    delete data with ORM

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
 // Controller
-// import controller function here 
-const { addUsers, getUsers, getUser, updateUser } = require('../controllers/user')
+// import controller function here
+const { addUsers, getUsers, getUser, updateUser, deleteUser } = require("../controllers/user");
 
 // Route
-router.post('/user', addUsers)
-router.get('/users', getUsers)
-router.get('/user/:id', getUser)
-router.patch('/user/:id', updateUser)
-// add route here
+router.post("/users", addUsers);
+router.get("/users", getUsers);
+router.get("/users/:id", getUser);
+router.patch("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
 
-module.exports = router
+module.exports = router;
