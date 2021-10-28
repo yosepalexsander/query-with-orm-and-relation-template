c271f33a6ba54e69fe38c05e33d74430baa5bea1
yosep <yosepalexsander9@gmail.com>
2021-10-28 09:51:57 +0700

    done fetching data with ORM

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,27 +1,62 @@
-const { user } = require('../../models')
+const { user } = require("../../models");
 
-exports.addUsers = async (req, res) => {
-    try {
+exports.addUser = async (req, res) => {
+  try {
+    await user.create(req.body);
 
-        await user.create(req.body)
-
-        res.send({
-            status: 'success',
-            message: 'Add user finished'
-        })
-    } catch (error) {
-        console.log(error)
-        res.send({
-            status: 'failed',
-            message: 'Server Error'
-        })
-    }
-}
+    res.send({
+      status: "success",
+      message: "Add user finished",
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: "failed",
+      message: "Server Error",
+    });
+  }
+};
 
 exports.getUsers = async (req, res) => {
-    // code here
-}
+  try {
+    const data = await user.findAll({
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    });
+    res.send({
+      status: "success",
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: "failed",
+      message: "Server Error",
+    });
+  }
+};
 
 exports.getUser = async (req, res) => {
-    // code here
-}
+  const { id } = req.params;
+  try {
+    const data = await user.findOne({
+      where: {
+        id,
+      },
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    });
+    res.send({
+      status: "success",
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: "failed",
+      message: "Server Error",
+    });
+  }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
 // Controller
 // import controller function here
-const { addUsers } = require('../controllers/user')
+const { addUser, getUser, getUsers } = require("../controllers/user");
 
 // Route
-router.post('/user', addUsers)
-// add route here
+router.post("/users", addUser);
+router.get("/users", getUsers);
+router.get("/users/:id", getUser);
 
-module.exports = router
+module.exports = router;
